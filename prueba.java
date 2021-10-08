c9239555c6550ea611785e8b394295140022004d
Barranco <alex@barranco.ws>
2021-10-08 11:28:16 +0200

    Cambiado Hola mundo por chau gente

--- prueba.java
+++ prueba.java
@@ -1,7 +1,7 @@
 public class TestClass {
 private final String message;
 public TestClass() {
-this.message = “Hola mundo”;
+this.message = “Chau gente”;
 }
 public String getMessage() {
 return message;
